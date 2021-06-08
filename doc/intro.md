3a9da1b8614b10d7b48100d0a8be424e31c97bcb
Ryan Griffiths <>
2021-06-09 00:46:26 +0100

    Swithcing to use leiningen for dependency management

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to aoc-clojure-leiningen
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- src/Day1/day1.clj
+++ src/Day1/day1.clj
@@ -1,28 +1,38 @@
-(ns Day1.day1 (:require [Utils.utils :as utils]))
+(ns Day1.day1
+  (:require [clojure.math.combinatorics :as combo])
+  (:require [Utils.utils :as utils]))
 
 (def lines (map #(Integer/parseInt %) (utils/readlines "resources/Day1/Input.txt")))
 
-(doseq [i lines]
-  (doseq [j lines]
-    (if (= 2020 (+ i j))
-      (def result1 (* i j))
+(defn findRemainder [val] (- 2020 val))
+
+(defn remainderInList [val] (some #(= (findRemainder val) %) lines))
+
+(defn pairSumRemainderInList [pair] (remainderInList (+ (nth pair 0) (nth pair 1))))
+
+(def result1
+  (first
+    (filter
+      #(remainderInList %)
+      lines
       )
     )
   )
 
 (print "part 1: ")
-(println result1)
+(println (* result1 (- 2020 result1)))
 
-
-(doseq [i lines]
-  (doseq [j lines]
-    (doseq [k lines]
-      (if (= 2020 (+ i j k))
-        (def result2 (* i j k))
-        )
+(def result2
+  (first
+    (filter
+      #(pairSumRemainderInList %)
+      (combo/combinations lines 2)
       )
     )
   )
 
+(def val1 (nth result2 0))
+(def val2 (nth result2 1))
+
 (print "part 2: ")
-(println result2)
+(println (* val1 val2 (- 2020 val1 val2)))
